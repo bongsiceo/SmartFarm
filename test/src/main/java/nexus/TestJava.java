03452cf58a67752b4ff2fd3071376c542d6c0c93
bongsiceo <bongsiceo@nate.com>
2016-01-08 16:16:53 +0900

    잘생김
    나는 잘생김

--- test/src/main/java/nexus/TestJava.java
+++ test/src/main/java/nexus/TestJava.java
@@ -5,6 +5,6 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("스프링 프로젝트를 github에 연결합니다.");
 		System.out.println("donghyun");
-	}
-
+		System.out.println("잘생김");
+}
 }
