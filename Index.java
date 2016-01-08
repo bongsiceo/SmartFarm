4b6244f81465e200c6fe98383f1ecdb653deccc5
BarkYongJun <elpincerss@gmail.com>
2016-01-08 15:46:49 +0900

    sts에서 작업함

--- Index.java
+++ Index.java
@@ -1,7 +1,3 @@
-//깃헙 사용법 테스트
-//작업하기 전에 누가 어디부터 어디까지, 어떤 내용의 코드로 작업했는지
-//주석 달아주세요
-//ex ) 박용준 2016-01-07 테스트 코드
 public class Index {
   private int indexer;
   private String name;
@@ -10,5 +6,6 @@ public class Index {
   public setIndexer(int indexer) {
     this indexer = indexer;
     System.out.println("일단 다녀갑니당");
+    System.out.println("스프링에서 작업함");
   }
 }
